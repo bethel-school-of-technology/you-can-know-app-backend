58e8eafab9cb86f8e57974c20aa38771ae1ab1b4
tanglo8 <manueldurbina@gmail.com>
2021-09-08 20:31:27 -0500

    added post and migration

---
+++ migrations/2-second_migration.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "posts", deps: []
+ *
+ **/
+
+var info = {
+    "revision": 2,
+    "name": "second_migration",
+    "created": "2021-09-09T01:21:34.117Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+    fn: "createTable",
+    params: [
+        "posts",
+        {
+            "PostId": {
+                "type": Sequelize.INTEGER,
+                "field": "PostId",
+                "primaryKey": true,
+                "autoIncrement": true,
+                "allowNull": false
+            },
+            "PostTitle": {
+                "type": Sequelize.STRING,
+                "field": "PostTitle"
+            },
+            "PostBody": {
+                "type": Sequelize.STRING,
+                "field": "PostBody"
+            },
+            "UserId": {
+                "type": Sequelize.INTEGER,
+                "field": "UserId",
+                "allowNull": false
+            },
+            "Delete": {
+                "type": Sequelize.BOOLEAN,
+                "field": "Delete"
+            },
+            "createdAt": {
+                "type": Sequelize.DATE,
+                "field": "createdAt",
+                "allowNull": false
+            },
+            "updatedAt": {
+                "type": Sequelize.DATE,
+                "field": "updatedAt",
+                "allowNull": false
+            }
+        },
+        {}
+    ]
+}];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
